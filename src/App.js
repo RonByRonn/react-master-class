6551f5c7a2c3bccd3c9b3305302c746ed70dc427
JW <ronbyronn@gmail.com>
2023-11-05 15:34:38 +0900

    2.4 Animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
-	display: flex;
+const rotation = keyframes`
+0% {
+  transform: rotate(0deg);
+  border-radius: 0px;
+}
+50% {
+  border-radius: 100px;
+}
+100% { 
+  transform: rotate(360deg);
+  border-radius: 0px;
+}
 `;
 
 const Box = styled.div`
-	background-color: ${(props) => props.bgColor};
-	width: 100px;
-	height: 100px;
-`;
-
-const Circle = styled(Box)`
-	border-radius: 50px;
-`;
-
-const Btn = styled.button`
-	color: white;
+	height: 200px;
+	width: 200px;
+	justify-content: center;
+	align-items: center;
 	background-color: tomato;
-	border: 0;
-	border-radius: 15px;
-`;
-
-const Input = styled.input.attrs({ required: true })`
-	background-color: green;
+	animation: ${rotation} linear infinite 2s;
+	display: flex;
+	span {
+		font-size: 80px;
+		&:hover {
+			font-size: 160px;
+		}
+		&:active {
+			opacity: 0;
+		}
+	}
 `;
 
 function App() {
 	return (
-		<Father>
-			{/* <Box bgColor="teal" />
-			<Circle bgColor="tomato" /> */}
-			<Btn>Login</Btn>
-			<Btn as="a" href="/">
-				Logout
-			</Btn>
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-			<Input />
-		</Father>
+		<div>
+			<Box>
+				<span>😎</span>
+			</Box>
+		</div>
 	);
 }
 
